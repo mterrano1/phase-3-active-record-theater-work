0ca0d500c0272db9af5f2124259c83f27a547574
Christopher Terrano <mterrano1@yahoo.com>
2022-04-14 14:40:31 -0500

    commit tree ab916a88f1ba3b3408e6b6614c09b9c374e5b385

---
+++ app/models/role.rb
@@ -0,0 +1,21 @@
+class Role < ActiveRecord::Base
+    has_many :auditions
+
+    def actors
+        self.auditions.map { |audition| audition.actor }
+    end
+
+    def locations
+        self.auditions.map { |audition| audition.location }
+    end
+
+    def lead
+        self.auditions.find_by(hired: true)
+        lead ? lead : "no actor has been hired for this role"
+    end
+
+    def understudy
+        self.auditions.where(hired: true).second
+        understudy ? understudy : "no actor has been hired for understudy for this role"
+    end
+end
